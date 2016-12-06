bfbd207f8a8e93aa70df93a0aa7afb78cfaf1c10
Joseph Tongay <dirk.tongay@gmail.com>
2016-12-05 20:10:51 -0600

    successfully added relationship between the 2 tables and added bootstrap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Posts.join(:users).select("posts.id, posts.title, posts.body, posts.image, users.username")
+    @posts = Posts.joins(:users).select("posts.id, posts.title, posts.body, posts.image, users.username")
 
     respond_to do |format|
       format.html
       format.json {render json: @posts}
     end
   end
+  
 end
