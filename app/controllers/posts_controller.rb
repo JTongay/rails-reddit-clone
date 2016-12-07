13ff4240c8a2a012bba64cf25244cda700ad02c8
Joseph Tongay <dirk.tongay@gmail.com>
2016-12-06 20:25:55 -0600

    trying to get the data the right way to display the home page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,19 @@
 class PostsController < ApplicationController
 
   def index
+    # @posts = Comments.joins(:users, :posts).select("posts.id, posts.title, posts.body, posts.image, users.username, comments.content")
     @posts = Posts.joins(:users).select("posts.id, posts.title, posts.body, posts.image, users.username")
+
+    @posts.each do |p|
+      puts p.inspect
+      @comments = Comments.joins(:posts).find(p.id)
+    end
+
 
     respond_to do |format|
       format.html
       format.json {render json: @posts}
     end
   end
-  
+
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,4 +4,8 @@ class PostsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "the truth" do
+    assert true
+  end
+  end
 end
