2e911263a9fa44e6fdaa456d9b853dadc309e8c4
Joseph Tongay <dirk.tongay@gmail.com>
2016-12-16 10:35:19 -0600

    more migration fixing

--- db/migrate/20161213201241_rename_post_posts_id_to_post_id.rb
+++ db/migrate/20161213201241_rename_post_posts_id_to_post_id.rb
@@ -1,7 +1,7 @@
 class RenamePostPostsIdToPostId < ActiveRecord::Migration
   def change
-    rename_column :comments, :posts_id, :post_id
-    rename_column :comments, :users_id, :user_id
-    rename_column :posts, :users_id, :user_id
+    # rename_column :comments, :posts_id, :post_id
+    # rename_column :comments, :users_id, :user_id
+    # rename_column :posts, :users_id, :user_id
   end
 end
